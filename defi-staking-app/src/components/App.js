03c7b9827114b0ee430b49e570820a8e4cc963d3
Nicholas Nikas <nicholasnikas@Nicholass-MacBook-Pro.local>
2022-02-13 12:20:42 -0500

    Add particles and staking and unstaking logic

--- defi-staking-app/src/components/App.js
+++ defi-staking-app/src/components/App.js
@@ -1,93 +1,173 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react'
 import './App.css'
 import NavBar from './NavBar.js'
 import Web3 from 'web3'
 import Tether from '../truffle_abis/Tether.json'
 import RWD from '../truffle_abis/RWD.json'
 import DecentralBank from '../truffle_abis/DecentralBank.json'
+import Main from './Main.js'
+import ParticleSettings from './ParticleSettings.js'
 
 class App extends Component {
+  async UNSAFE_componentWillMount() {
+    await this.loadWeb3()
+    await this.loadBlockchainData()
+  }
 
-    async UNSAFE_componentWillMount() {
-        await this.loadWeb3()
-        await this.loadBlockchainData()
+  async loadWeb3() {
+    if (window.ethereum) {
+      window.web3 = new Web3(window.ethereum)
+      await window.ethereum.enable()
+    } else if (window.web3) {
+      window.web3 = new Web3(window.web3.currentProvider)
+    } else {
+      window.alert('No ethereum browser detected! You can check MetaMask!')
+    }
+  }
+
+  async loadBlockchainData() {
+    const web3 = window.web3
+    const account = await web3.eth.getAccounts()
+    this.setState({ account: account[0] })
+    const networkId = await web3.eth.net.getId()
+
+    // load Tether contract
+    const tetherData = Tether.networks[networkId]
+    if (tetherData) {
+      const tether = new web3.eth.Contract(Tether.abi, tetherData.address)
+      this.setState({ tether })
+      let tetherBalance = await tether.methods
+        .balanceOf(this.state.account)
+        .call()
+      this.setState({ tetherBalance: tetherBalance.toString() })
+    } else {
+      window.alert('Error! Tether contract not deployed - no detected network!')
     }
 
-    async loadWeb3() {
-        if(window.ethereum) {
-            window.web3 = new Web3(window.ethereum)
-            await window.ethereum.enable()
-        } else if(window.web3) {
-            window.web3 = new Web3(window.web3.currentProvider)
-        } else {
-            window.alert('No ethereum browser detected! You can check MetaMask!')
-        }
+    // load RWD contract
+    const rwdData = RWD.networks[networkId]
+    if (rwdData) {
+      const rwd = new web3.eth.Contract(RWD.abi, rwdData.address)
+      this.setState({ rwd })
+      let rwdBalance = await rwd.methods.balanceOf(this.state.account).call()
+      this.setState({ rwdBalance: rwdBalance.toString() })
+    } else {
+      window.alert('Error! RWD contract not deployed - no detected network!')
     }
 
-    async loadBlockchainData() {
-        const web3 = window.web3
-        const account = await web3.eth.getAccounts()
-        this.setState({account: account[0]})
-        const networkId = await web3.eth.net.getId()
-
-        // load Tether contract
-        const tetherData = Tether.networks[networkId]
-        if(tetherData) {
-            const tether = new web3.eth.Contract(Tether.abi, tetherData.address)
-            this.setState({tether})
-            let tetherBalance = await tether.methods.balanceOf(this.state.account).call()
-            this.setState({tetherBalance: tetherBalance.toString()})
-        } else {
-            window.alert('Error! Tether contract not deployed - no detected network!')
-        }
-
-        // load RWD contract
-        const rwdData = RWD.networks[networkId]
-        if(rwdData) {
-            const rwd = new web3.eth.Contract(RWD.abi, rwdData.address)
-            this.setState({rwd})
-            let rwdBalance = await rwd.methods.balanceOf(this.state.account).call()
-            this.setState({rwdBalance: rwdBalance.toString()})
-        } else {
-            window.alert('Error! RWD contract not deployed - no detected network!')
-        }
-
-        // load DecentralBank bank
-        const decentralBankData = DecentralBank.networks[networkId]
-        if(decentralBankData) {
-            const decentralBank = new web3.eth.Contract(DecentralBank.abi, decentralBankData.address)
-            this.setState({decentralBank})
-            let stakingBalance = await decentralBank.methods.stakingBalance(this.state.account).call()
-            this.setState({stakingBalance: stakingBalance.toString()})
-        } else {
-            window.alert('Error! DecentralBank contract not deployed - no detected network!')
-        }
-
-        this.setState({loading: false})
+    // load DecentralBank bank
+    const decentralBankData = DecentralBank.networks[networkId]
+    if (decentralBankData) {
+      const decentralBank = new web3.eth.Contract(
+        DecentralBank.abi,
+        decentralBankData.address
+      )
+      this.setState({ decentralBank })
+      let stakingBalance = await decentralBank.methods
+        .stakingBalance(this.state.account)
+        .call()
+      this.setState({ stakingBalance: stakingBalance.toString() })
+    } else {
+      window.alert(
+        'Error! DecentralBank contract not deployed - no detected network!'
+      )
     }
 
-    constructor(props) {
-        super(props)
-        this.state = {
-            account: '0x0',
-            tether: {},
-            rwd: {},
-            decentralBank: {},
-            tetherBalance: '0',
-            rwdBalance: '0',
-            stakingBalance: '0',
-            loading: true,
-        }
-    }
+    this.setState({ loading: false })
+  }
 
-    render() {
-        const { account } = this.state;
-        return (
-            <div>
-                <NavBar account={account}/>
-            </div>
-        )
+  // staking function
+  stakeTokens = (amount) => {
+    const { tether, decentralBank, account } = this.state
+    this.setState({ loading: true })
+
+    tether.methods
+      .approve(decentralBank._address, amount)
+      .send({ from: this.state.account })
+      .on('transactionHash', (hash) => {
+        decentralBank.methods
+          .depositTokens(amount)
+          .send({ from: account })
+          .on('transactionHash', (hash) => {
+            this.setState({ loading: false })
+          })
+      })
+  }
+
+  // unstaking function
+  unstakeTokens = () => {
+    const { decentralBank, account } = this.state
+    this.setState({ loading: true })
+
+    decentralBank.methods
+      .unstakeTokens()
+      .send({ from: account })
+      .on('transactionHash', (hash) => {
+        this.setState({ loading: false })
+      })
+  }
+
+  constructor(props) {
+    super(props)
+    this.state = {
+      account: '0x0',
+      tether: {},
+      rwd: {},
+      decentralBank: {},
+      tetherBalance: '0',
+      rwdBalance: '0',
+      stakingBalance: '0',
+      loading: true,
     }
+  }
+
+  render() {
+    const {
+      account,
+      loading,
+      tetherBalance,
+      rwdBalance,
+      stakingBalance,
+    } = this.state
+
+    return (
+      <div className='App' style={{ position: 'relative' }}>
+        <div style={{ position: 'absolute' }}>
+          <ParticleSettings />
+        </div>
+        <NavBar account={account} />
+        <div className='container-fluid mt-5'>
+          <div className='row'>
+            <main
+              role='main'
+              className='col-lg-12 ml-auto mr-auto'
+              style={{ maxWidth: '600px', minHeight: '100vm' }}
+            >
+              <div>
+                {loading ? (
+                  <p
+                    id='loader'
+                    className='text-center'
+                    style={{ margin: '30px', color: 'white' }}
+                  >
+                    LOADING...
+                  </p>
+                ) : (
+                  <Main
+                    tetherBalance={tetherBalance}
+                    rwdBalance={rwdBalance}
+                    stakingBalance={stakingBalance}
+                    stakeTokens={this.stakeTokens}
+                    unstakeTokens={this.unstakeTokens}
+                  />
+                )}
+              </div>
+            </main>
+          </div>
+        </div>
+      </div>
+    )
+  }
 }
 
-export default App;
+export default App
---
+++ defi-staking-app/src/components/Main.js
@@ -0,0 +1,91 @@
+import React, { Component } from 'react'
+import tether from '../tether.png'
+
+class Main extends Component {
+  render() {
+    const {
+      tetherBalance,
+      rwdBalance,
+      stakingBalance,
+      stakeTokens,
+      unstakeTokens,
+    } = this.props
+    return (
+      <div id='content' className='mt-3'>
+        <table className='table text-muted text-center'>
+          <thead>
+            <tr style={{ color: 'white' }}>
+              <th scope='col'>Staking Balance</th>
+              <th scope='col'>Reward Balance</th>
+            </tr>
+          </thead>
+          <tbody style={{ color: 'white' }}>
+            <tr>
+              <td>{window.web3.utils.fromWei(stakingBalance, 'Ether')} USDT</td>
+              <td>{window.web3.utils.fromWei(rwdBalance, 'Ether')} RWD</td>
+            </tr>
+          </tbody>
+        </table>
+        <div className='card mb-2' style={{ opacity: '.9' }}>
+          <form
+            onSubmit={(event) => {
+              event.preventDefault()
+              const amount = window.web3.utils.toWei(
+                this.input.value.toString(),
+                'Ether'
+              )
+              stakeTokens(amount)
+            }}
+            className='mb-3'
+          >
+            <div style={{ borderSpacing: '0 1em' }}>
+              <label className='float-left' style={{ marginLeft: '15px' }}>
+                <b>Stake Tokens</b>
+              </label>
+              <span className='float-right' style={{ marginRight: '8px' }}>
+                Balance: {window.web3.utils.fromWei(tetherBalance)}
+              </span>
+              <div className='input-group mb-4'>
+                <input
+                  ref={(input) => {
+                    this.input = input
+                  }}
+                  type='text'
+                  placeholder='0'
+                  required
+                />
+                <div className='input-group-open'>
+                  <div className='input-group-text'>
+                    <img src={tether} alt='tether' height='32' />
+                    &nbsp;&nbsp;&nbsp;USDT
+                  </div>
+                </div>
+              </div>
+              <button
+                type='submit'
+                className='btn btn-primary btn-lg btn-block'
+              >
+                DEPOSIT
+              </button>
+            </div>
+          </form>
+          <button
+            className='btn btn-primary btn-lg btn-block'
+            type='submit'
+            onClick={(event) => {
+              event.preventDefault()
+              unstakeTokens()
+            }}
+          >
+            WITHDRAW
+          </button>
+          <div className='card-body text-center' style={{ color: 'blue' }}>
+            AIRDROP
+          </div>
+        </div>
+      </div>
+    )
+  }
+}
+
+export default Main
